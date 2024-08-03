69f2db1bbcf3c9bdc0e05a5a624feb44dfd7a55a
Sonu Kumar <128314830+sonukds@users.noreply.github.com>
2024-08-03 21:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+document.addEventListener('DOMContentLoaded', () => {
+    const evaluatedText = document.getElementById('evaluatedText');
+    const letterCount = document.getElementById('letterCount');
+
+    evaluatedText.addEventListener('input', () => {
+        const textLength = evaluatedText.value.length;
+        letterCount.textContent = textLength;
+    });
+});
